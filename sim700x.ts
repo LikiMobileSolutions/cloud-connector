d469d28bfb1bb739930bdef3fa90d0728da29265
kermit213 <krzysiek.kr6@wp.pl>
2019-12-04 14:34:46 +0100

    rename main file and small tweaks

---
+++ sim700x.ts
@@ -0,0 +1,16 @@
+/**
+ * SIM700x block
+ */
+
+//% color=#5042f4 icon="\uf2c9"
+namespace SIM700x {
+
+    //% weight=200 blockId="SendATCommand" 
+    //% block="SendATCommand %atCommand"
+    export function Temperature(atCommand: string): string {
+        // Fake function for testing
+        return "test"
+    }
+    
+    
+}
