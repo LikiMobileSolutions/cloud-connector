2c260b85454004d67096fdc527a2b22ac486013e
krzysztofkrzeslak <31014560+krzysztofkrzeslak@users.noreply.github.com>
2019-12-04 14:46:11 +0100

    Update sim700x.ts

--- sim700x.ts
+++ sim700x.ts
@@ -5,9 +5,9 @@
 //% color=#5042f4 icon="\uf2c9"
 namespace SIM700x {
 
-    //% weight=200 blockId="SendATCommand" 
+    //% weight=100 blockId="SendATCommand" 
     //% block="SendATCommand %atCommand"
-    export function Temperature(atCommand: string): string {
+    export function SendATCommand(atCommand: string): string {
         // Fake function for testing
         return "test"
     }
