ceb7a8c2d36a078eff9c4df0f0db017057626737
krzysztofkrzeslak <31014560+krzysztofkrzeslak@users.noreply.github.com>
2019-12-04 14:12:03 +0100

    initial test

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+//% color=#5042f4 icon="\uf2c9"
+namespace SIM700x {
+    /**
+     */
+     
+    //% shim=SIM700x::SendATCommand
+    export function SendATCommand(p: string): string {
+        // Fake function for simulator
+        return "test"
+    }
+    
+
+}
