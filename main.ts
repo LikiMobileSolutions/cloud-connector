b86b30a719eaed5e4678e9b59a43c5a7124c211a
krzysztofkrzeslak <31014560+krzysztofkrzeslak@users.noreply.github.com>
2019-12-04 14:24:49 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 //% color=#5042f4 icon="\uf2c9"
 namespace SIM700x {
-    /**
-     */
-     
-    //% shim=SIM700x::SendATCommand
-    export function SendATCommand(p: string): string {
+
+    //% weight=10 blockId="SendATCommand" 
+    //% block="Send AT Command %atCommand"
+    //% shim=DS18B20::Temperature
+    export function Temperature(atCommand: string): string {
         // Fake function for simulator
         return "test"
     }
     
-
+    
 }
