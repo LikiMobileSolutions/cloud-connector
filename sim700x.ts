60664adefa8f0b720a189efe0f89fbe38ff23266
krzysztofkrzeslak <krzysiek.kr6@wp.pl>
2019-12-05 10:30:58 +0100

    Organised blocks into groups

--- sim700x.ts
+++ sim700x.ts
@@ -14,7 +14,7 @@ namespace SIM700x {
     	*/
 	//% weight=100 blockId="SIM700Setup" 
 	//% block="SIM700x Setup RX: %SIM700RX_Pin TX: %SIM700TX_Pin Baud:%SIM700BaudRate"
-	//% SIM700TX_Pin.defl=SerialPin.P1 SIM700RX_Pin.defl=SerialPin.P0 SIM700BaudRate.defl=BaudRate.BaudRate115200
+	//% SIM700TX_Pin.defl=SerialPin.P1 SIM700RX_Pin.defl=SerialPin.P0 SIM700BaudRate.defl=BaudRate.BaudRate115200 group="1. Setup: "
 	export function Setup(SIM700TX_Pin: SerialPin, SIM700RX_Pin: SerialPin, SIM700BaudRate: BaudRate) {
 		_SIM700RX_Pin=SIM700RX_Pin
 		_SIM700TX_Pin=SIM700TX_Pin
@@ -27,6 +27,7 @@ namespace SIM700x {
     	*/
 	//% weight=100 blockId="SendATCommand" 
 	//% block="SIM700x SendATCommand %atCommand"
+	//% group="4. Low level functions:"
 	export function SendATCommand(atCommand: string): string {
 		serial.redirect(_SIM700RX_Pin,_SIM700TX_Pin,BaudRate.BaudRate115200)
 		serial.setWriteLinePadding(0)
@@ -37,10 +38,12 @@ namespace SIM700x {
 	}
 
 	/**
-    	* get signal strength, returns it in 0-4 range or -1 if something is wrong and signal can't be fetched
+    	* get signal strength,
+	*  return in 0-4 range
+	*  return -1 if something is wrong and signal can't be fetched
     	*/
 	//% weight=100 blockId="getSignalQuality" 
-	//% block="SIM700x GetSignalQuality"
+	//% block="SIM700x GetSignalQuality" group="2. Basic: "
 	export function getSignalQuality(): number {
 		let signalStrengthRaw = SendATCommand("AT+CSQ")
 		let signalStrengthLevel = -1
@@ -50,14 +53,17 @@ namespace SIM700x {
 			if(parseInt(signalStrengthRaw) != 99){ // 99 means that signal can't be fetched
 				signalStrengthLevel = Math.round(Math.map(parseInt(signalStrengthRaw), 0, 31, 0, 4))
 			}
-			
 		}
 		return signalStrengthLevel
 	}
 
-
+	/**
+    	*  Send sms message
+	*  Phone number must be in format: "+(country code)(9-digit phone number)" eg. +48333222111
+    	*/
 	//% weight=100 blockId="sendSmsMessage" 
 	//% block="SIM700x sendSmsMessage to: %phone_num, content: %content "
+	//% block="SIM700x GetSignalQuality" group="3. GSM: "
 	export function sendSmsMessage(phone_num: string, content: string): string {
 		let modemResponse=""
 		SendATCommand("AT+CMGF=1")
