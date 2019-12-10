f45ad53c8b933baaba0f509a6de4c693d9654c5e
krzysiek <krzysiek.kr6@wp.pl>
2019-12-10 21:21:13 +0100

    Changed formating of live object to handle multiple arguments

--- sim700x.ts
+++ sim700x.ts
@@ -4,16 +4,16 @@
 
 //% color=#5042f4 icon="\uf093"
 namespace SIM700x {
-	
+
 	let _SIM700TX_Pin=SerialPin.P1
 	let _SIM700RX_Pin=SerialPin.P0
 	let _SIM700BaudRate=BaudRate.BaudRate115200
 
 
-	
+
 	/**
     	* (internal function)
-    	*/	
+    	*/
 	function _SendATCommand(atCommand: string, timeout=1000): string {
 		serial.redirect(_SIM700RX_Pin, _SIM700TX_Pin, _SIM700BaudRate)
 	    	serial.setWriteLinePadding(0)
@@ -30,11 +30,11 @@ namespace SIM700x {
 	    	}
 	    return buffer //timeout exceeded, anyway return buffer
 	}
-	
+
 	/**
-    	* Init module 
+    	* Init module
     	*/
-	//% weight=100 blockId="SIM700Init" 
+	//% weight=100 blockId="SIM700Init"
 	//% block="SIM700x Init RX: %SIM700RX_Pin TX: %SIM700TX_Pin Baud:%SIM700BaudRate"
 	//% SIM700TX_Pin.defl=SerialPin.P1 SIM700RX_Pin.defl=SerialPin.P0 SIM700BaudRate.defl=BaudRate.BaudRate115200 group="1. Setup: "
 	export function Init(SIM700TX_Pin: SerialPin, SIM700RX_Pin: SerialPin, SIM700BaudRate: BaudRate) {
@@ -52,13 +52,13 @@ namespace SIM700x {
 	}
 
 
-	
+
 	/**
     	* get signal strength,
 	* return in 0-4 range
 	* return -1 if something is wrong and signal can't be fetched
     	*/
-	//% weight=100 blockId="getSignalQuality" 
+	//% weight=100 blockId="getSignalQuality"
 	//% block="SIM700x GetSignalQuality" group="2. Status: "
 	export function getSignalQuality(): number {
 		let signalStrengthRaw = _SendATCommand("AT+CSQ")
@@ -73,11 +73,11 @@ namespace SIM700x {
 		return signalStrengthLevel
 	}
 
-	
+
 	/**
 	* return gsm network registration status as code, 1 or 5 mean sucessfull registartion
     	*/
-	//% weight=100 blockId="getGSMRegistrationStatus" 
+	//% weight=100 blockId="getGSMRegistrationStatus"
 	//% block="SIM700x GetGSMRegistrationStatus" group="2. Status: "
 	export function getGSMRegistrationStatus(): number {
 		let response = _SendATCommand("AT+CREG?")
@@ -94,7 +94,7 @@ namespace SIM700x {
 	/**
     	* Network init
     	*/
-	//% weight=100 blockId="SIM700InitNetwork" 
+	//% weight=100 blockId="SIM700InitNetwork"
 	//% block="SIM700x network init: APNname:%ApnName" group="4. Network:"
 	export function InitNetwork(ApnName: string) {
 
@@ -121,7 +121,7 @@ namespace SIM700x {
 	/**
     	* MQTT init
     	*/
-	//% weight=100 blockId="SIM700InitMQTT" 
+	//% weight=100 blockId="SIM700InitMQTT"
 	//% block="SIM700x MQTT init BrokerUrl:%brokerUrl brokerPort:%brokerPort clientId:%clientId username:%username passwd:%password" group="4. Network:"
 	export function InitMQTT(brokerUrl: string, brokerPort: string, clientId: string, username: string, password: string) {
 		_SendATCommand('AT+SMCONF="URL","'+brokerUrl+'","'+brokerPort+'"')
@@ -134,7 +134,7 @@ namespace SIM700x {
 	/**
     	* MQTT publish message
     	*/
-	//% weight=100 blockId="SIM700MqttPublish" 
+	//% weight=100 blockId="SIM700MqttPublish"
 	//% block="SIM700x MQTT publish topic:%brokerUrl message:%message" group="4. Network:"
 	export function MqttPublish(topic: string, message: string) {
 		SendATCommand('AT+SMPUB="'+topic+'",' + message.length + ',0,1')
@@ -144,18 +144,26 @@ namespace SIM700x {
 	/**
     	* MQTT live object publish message
     	*/
-	//% weight=100 blockId="SIM700MqttLiveObjectPublish" 
-	//% block="SIM700x Live object publish stream:%stream value:%value" group="4. Network:"
-	export function LiveObjectPublish(stream: string, value: string) {
-		let liveObjectMsg = '{ "s":"'+stream+'", "v": { "timestamp": "2019-12-09 09:00:00", "value":"'+value+'"} }'
+	//% weight=100 blockId="SIM700MqttLiveObjectPublish"
+	//% block="SIM700x Live object publish stream:%stream data:%data" group="4. Network:"
+	export function LiveObjectPublish(stream: string, data: string[]) {
+		let dataString = ''
+		if(data.length != 0){
+			dataString+=","
+			for(let i=0; i<data.length; i++){
+	    	dataString+='"'+i+'":"'+data[i]+'"'
+			}
+		}
+
+		let liveObjectMsg = '{ "s":"'+stream+'", "v": { "timestamp": "2100-01-01 09:00:00"'+dataString+'} }'
 		MqttPublish("dev/data",liveObjectMsg)
 	}
 
 
 	/**
-    	* 
+    	*
     	*/
-	//% weight=100 blockId="SIM700USBSerialLog" 
+	//% weight=100 blockId="SIM700USBSerialLog"
 	//% block="USBSerialLog %message"
 	//% group="5. Low level  and debug functions:"
 	export function USBSerialLog(message: string) {
@@ -166,7 +174,7 @@ namespace SIM700x {
 	/**
     	* Send plain AT command to modem and return response from it
     	*/
-	//% weight=100 blockId="SendATCommand" 
+	//% weight=100 blockId="SendATCommand"
 	//% block="SIM700x SendATCommand %atCommand"
 	//% group="5. Low level  and debug functions:"
 	export function SendATCommand(atCommand: string): string {
@@ -176,7 +184,7 @@ namespace SIM700x {
 	/**
     	* Send plain AT command to modem wait for response up to timeout and return modem response
     	*/
-	//% weight=100 blockId="SendATCommandSetTimeout" 
+	//% weight=100 blockId="SendATCommandSetTimeout"
 	//% block="SIM700x SendATCommand %atCommand timeout: %timeout"
 	//% timeout.defl=100 group="5. Low level  and debug functions:"
 	export function SendATCommandSetTimeout(atCommand: string, timeout: number): string {
@@ -186,7 +194,7 @@ namespace SIM700x {
 	/**
     	* Check if module is up and responding properly to AT command
     	*/
-	//% weight=100 blockId="isEnabled" 
+	//% weight=100 blockId="isEnabled"
 	//% block="SIM700x isEnabled" group="2. Status: "
 	export function isEnabled(): boolean {
 		let atResponse = _SendATCommand("AT")
@@ -201,14 +209,14 @@ namespace SIM700x {
     	*  Send sms message
 	*  Phone number must be in format: "+(country code)(9-digit phone number)" eg. +48333222111
     	*/
-	//% weight=100 blockId="sendSmsMessage" 
+	//% weight=100 blockId="sendSmsMessage"
 	//% block="SIM700x sendSmsMessage to: %phone_num, content: %content " group="3. GSM: "
 	export function sendSmsMessage(phone_num: string, content: string) {
 		SendATCommand("AT+CMGF=1") // set text mode
-		SendATCommand('AT+CMGS="' + phone_num + '"') 
+		SendATCommand('AT+CMGS="' + phone_num + '"')
 		SendATCommand(content + "\x1A")
 	}
 
 
-    
+
 }
