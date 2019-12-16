6ccec5ce57826f16e1536c283ff820eca6d5afa5
krzysiek <krzysiek.kr6@wp.pl>
2019-12-16 19:09:13 +0100

    Set mqtt conection to utilize AT command sending with checking response

--- sim700x.ts
+++ sim700x.ts
@@ -155,11 +155,11 @@ namespace SIM700x {
 	//% weight=100 blockId="SIM700InitMQTT"
 	//% block="SIM700x MQTT init BrokerUrl:%brokerUrl brokerPort:%brokerPort clientId:%clientId username:%username passwd:%password" group="4. Network:"
 	export function InitMQTT(brokerUrl: string, brokerPort: string, clientId: string, username: string, password: string) {
-		_SendATCommand('AT+SMCONF="URL","'+brokerUrl+'","'+brokerPort+'"')
-		_SendATCommand('AT+SMCONF="CLIENTID","'+clientId+'"')
-		_SendATCommand('AT+SMCONF="USERNAME","'+username+'"')
-		_SendATCommand('AT+SMCONF="PASSWORD","'+password+'"')
-		_SendATCommand("AT+SMCONN", -1)
+		_SendATCommandCheckACK('AT+SMCONF="URL","'+brokerUrl+'","'+brokerPort+'"')
+		_SendATCommandCheckACK('AT+SMCONF="CLIENTID","'+clientId+'"')
+		_SendATCommandCheckACK('AT+SMCONF="USERNAME","'+username+'"')
+		_SendATCommandCheckACK('AT+SMCONF="PASSWORD","'+password+'"')
+		_SendATCommandCheckACK("AT+SMCONN")
 	}
 
 	/**
