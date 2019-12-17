7b738db0a36348ce89a0729a5b2e958fd8047cd8
krzysztofkrzeslak <krzysiek.kr6@wp.pl>
2019-12-17 10:23:41 +0100

    code cleanup(line indents)

--- sim700x.ts
+++ sim700x.ts
@@ -12,130 +12,124 @@ namespace SIM700x {
 
 
 	/**
-    	* (internal function)
-    	*/
+	* (internal function)
+	*/
 	function _SendATCommand(atCommand: string, timeout=1000, useNewLine=true): string {
-		//serial.redirect(_SIM700RX_Pin, _SIM700TX_Pin, _SIM700BaudRate)
-	    	//serial.setWriteLinePadding(0)
-	    	//serial.setRxBufferSize(128)
-				if(useNewLine){
-					serial.writeLine(atCommand)
-				}else{
-					serial.writeString(atCommand)
-				}
-
-
-	    	let startTs = input.runningTime()
-	    	let buffer = ""
-	    	while ( (input.runningTime() - startTs <= timeout) || (timeout==-1) ) { //read until timeout is not exceeded
-			buffer += serial.readString()
-			if (buffer.includes("OK") || buffer.includes("ERROR")) { //command completed, modem responded
-		    		return buffer
+			if(useNewLine){
+				serial.writeLine(atCommand)
+			}else{
+				serial.writeString(atCommand)
 			}
-	    	}
-	    return buffer //timeout exceeded, anyway return buffer
-	}
 
-	/**
-			* (internal function)
-			*/
-	function _SendATCommandCheckACK(atCommand: string, limit=5): boolean {
-		let modemResponse = _SendATCommand(atCommand,-1)
-		let tries=0
-		while(modemResponse.includes("ERROR")){
-				if(tries>limit){
-					return false
+			let startTs = input.runningTime()
+			let buffer = ""
+			while ( (input.runningTime() - startTs <= timeout) || (timeout==-1) ) { //read until timeout is not exceeded
+				buffer += serial.readString()
+				if (buffer.includes("OK") || buffer.includes("ERROR")) { //command completed, modem responded
+		  		return buffer
 				}
-				modemResponse = _SendATCommand(atCommand,-1)
-				basic.pause(100*tries) //adaptively extend pause during sending commands which fail
-				tries++
-
-		}
-		return true
+			}
+		return buffer //timeout exceeded, anyway return buffer
 	}
 
 	/**
-    	* Init module
-    	*/
+	* (internal function)
+	*/
+	function _SendATCommandCheckACK(atCommand: string, limit=5): boolean {
+			let modemResponse = _SendATCommand(atCommand,-1)
+			let tries=0
+			while(modemResponse.includes("ERROR")){
+					if(tries>limit){
+						return false
+					}
+					modemResponse = _SendATCommand(atCommand,-1)
+					basic.pause(100*tries) //adaptively extend pause during sending commands which fail
+					tries++
+
+			}
+			return true
+	}
+
+	/**
+	* Init module
+	*/
 	//% weight=100 blockId="SIM700Init"
 	//% block="SIM700x Init RX: %SIM700RX_Pin TX: %SIM700TX_Pin Baud:%SIM700BaudRate"
 	//% SIM700TX_Pin.defl=SerialPin.P1 SIM700RX_Pin.defl=SerialPin.P0 SIM700BaudRate.defl=BaudRate.BaudRate115200 group="1. Setup: "
 	export function Init(SIM700TX_Pin: SerialPin, SIM700RX_Pin: SerialPin, SIM700BaudRate: BaudRate) {
-		_SIM700RX_Pin=SIM700RX_Pin
-		_SIM700TX_Pin=SIM700TX_Pin
-		_SIM700BaudRate=SIM700BaudRate
+			_SIM700RX_Pin=SIM700RX_Pin
+			_SIM700TX_Pin=SIM700TX_Pin
+			_SIM700BaudRate=SIM700BaudRate
 
-		serial.redirect(_SIM700RX_Pin, _SIM700TX_Pin, _SIM700BaudRate)
-		serial.setWriteLinePadding(0)
-		serial.setRxBufferSize(128)
+			serial.redirect(_SIM700RX_Pin, _SIM700TX_Pin, _SIM700BaudRate)
+			serial.setWriteLinePadding(0)
+			serial.setRxBufferSize(128)
 
-		let atResponse = _SendATCommand("AT")
-		let checks=0
-		while( !(atResponse.includes("AT") && atResponse.includes("OK")) ){ //check in loop if echo is enabled
-			_SendATCommand("ATE 1")
-			atResponse = _SendATCommand("AT",1000)
-			if(checks>=10){
-				break;
+			let atResponse = _SendATCommand("AT")
+			let checks=0
+			while( !(atResponse.includes("AT") && atResponse.includes("OK")) ){ //check in loop if echo is enabled
+				_SendATCommand("ATE 1")
+				atResponse = _SendATCommand("AT",1000)
+				if(checks>=10){
+					break;
+				}
+				checks++
 			}
-			checks++
-		}
 	}
 
-
-
 	/**
-    	* get signal strength,
+	* get signal strength,
 	* return in 0-4 range
 	* return -1 if something is wrong and signal can't be fetched
-    	*/
+	*/
 	//% weight=100 blockId="getSignalQuality"
 	//% block="SIM700x GetSignalQuality" group="2. Status: "
 	export function getSignalQuality(): number {
-		let signalStrengthRaw = _SendATCommand("AT+CSQ")
-		let signalStrengthLevel = -1
-		if (signalStrengthRaw.includes("+CSQ:")) {
-			signalStrengthRaw = signalStrengthRaw.split(": ")[1]
-			signalStrengthRaw = signalStrengthRaw.split(",")[0]
-			if(parseInt(signalStrengthRaw) != 99){ // 99 means that signal can't be fetched
-				signalStrengthLevel = Math.round(Math.map(parseInt(signalStrengthRaw), 0, 31, 0, 4))
+			let signalStrengthRaw = _SendATCommand("AT+CSQ")
+			let signalStrengthLevel = -1
+			if (signalStrengthRaw.includes("+CSQ:")) {
+				signalStrengthRaw = signalStrengthRaw.split(": ")[1]
+				signalStrengthRaw = signalStrengthRaw.split(",")[0]
+				if(parseInt(signalStrengthRaw) != 99){ // 99 means that signal can't be fetched
+					signalStrengthLevel = Math.round(Math.map(parseInt(signalStrengthRaw), 0, 31, 0, 4))
+				}
 			}
-		}
-		return signalStrengthLevel
+			return signalStrengthLevel
 	}
 
 
 	/**
 	* return gsm network registration status as code, 1 or 5 mean sucessfull registartion
-    	*/
+	*/
 	//% weight=100 blockId="getGSMRegistrationStatus"
 	//% block="SIM700x GetGSMRegistrationStatus" group="2. Status: "
 	export function getGSMRegistrationStatus(): number {
-		let response = _SendATCommand("AT+CREG?")
-		let registrationStatusCode = -1;
-		if (response.includes("+CREG:")) {
-			response = response.split(",")[1]
-			registrationStatusCode = parseInt(response.split("\r\n")[0])
+			let response = _SendATCommand("AT+CREG?")
+			let registrationStatusCode = -1;
+			if (response.includes("+CREG:")) {
+				response = response.split(",")[1]
+				registrationStatusCode = parseInt(response.split("\r\n")[0])
 
-		}
-		return registrationStatusCode
+			}
+			return registrationStatusCode
 	}
 
 	/**
-    	*  Send sms message
+	*  Send sms message
 	*  Phone number must be in format: "+(country code)(9-digit phone number)" eg. +48333222111
-    	*/
+	*/
 	//% weight=100 blockId="sendSmsMessage"
 	//% block="SIM700x sendSmsMessage to: %phone_num, content: %content " group="3. GSM: "
 	export function sendSmsMessage(phone_num: string, content: string) {
-		_SendATCommand("AT+CMGF=1") // set text mode
-		_SendATCommand('AT+CMGS="' + phone_num + '"')
-		_SendATCommand(content + "\x1A")
+			_SendATCommand("AT+CMGF=1") // set text mode
+			_SendATCommand('AT+CMGS="' + phone_num + '"')
+			_SendATCommand(content + "\x1A")
 	}
 
 
 	/**
-    	* Network init
-    	*/
+	* Network init
+	*/
 	//% weight=100 blockId="SIM700InitNetwork"
 	//% block="SIM700x network init: APNname:%ApnName" group="4. Network:"
 	export function InitNetwork(ApnName: string) {
@@ -161,8 +155,8 @@ namespace SIM700x {
 	}
 
 	/**
-    	* MQTT init
-    	*/
+	* MQTT init
+	*/
 	//% weight=100 blockId="SIM700InitMQTT"
 	//% block="SIM700x MQTT init BrokerUrl:%brokerUrl brokerPort:%brokerPort clientId:%clientId username:%username passwd:%password" group="4. Network:"
 	export function InitMQTT(brokerUrl: string, brokerPort: string, clientId: string, username: string, password: string) {
@@ -177,34 +171,34 @@ namespace SIM700x {
 	}
 
 	/**
-    	* MQTT publish message
-    	*/
+	* MQTT publish message
+	*/
 	//% weight=100 blockId="SIM700MqttPublish"
 	//% block="SIM700x MQTT publish topic:%brokerUrl message:%message || qos:%qos retain:%retain" group="4. Network:"
 	//% qos.defl=0 retain.defl=0 expandableArgumentMode="toggle"
 	export function MqttPublish(topic: string, message: string, qos=2, retain=0) {
-		let cmd='AT+SMPUB="'+topic+'",' + message.length + ','+qos+','+retain
-		_SendATCommand(cmd,100)
-		let modemResponse=_SendATCommand(message,3000,false)
-
-		let tries=0
-		while(modemResponse.includes("ERROR") && (!(tries>5)) ){
-			//try to reconnect mqtt
-			_SendATCommand("AT+SMDISC",-1)
-			_SendATCommand("AT+SMCONN",-1)
-
-			//retry
+			let cmd='AT+SMPUB="'+topic+'",' + message.length + ','+qos+','+retain
 			_SendATCommand(cmd,100)
-			modemResponse=_SendATCommand(message,5000,false)
+			let modemResponse=_SendATCommand(message,3000,false)
 
-			tries++
-		}
+			let tries=0
+			while(modemResponse.includes("ERROR") && (!(tries>5)) ){
+				//try to reconnect mqtt
+				_SendATCommand("AT+SMDISC",-1)
+				_SendATCommand("AT+SMCONN",-1)
+
+				//retry
+				_SendATCommand(cmd,100)
+				modemResponse=_SendATCommand(message,5000,false)
+
+				tries++
+			}
 
 	}
 
 	/**
-    	* MQTT live object publish message
-    	*/
+	* MQTT live object publish message
+	*/
 	//% weight=100 blockId="SIM700MqttLiveObjectPublish"
 	//% block="SIM700x Live object publish stream:%stream data:%data" group="4. Network:"
 	export function LiveObjectPublish(stream: string, data: string[]) {
@@ -220,8 +214,8 @@ namespace SIM700x {
 
 
 	/**
-    	*
-    	*/
+	*
+	*/
 	//% weight=100 blockId="SIM700USBSerialLog"
 	//% block="USBSerialLog %message"
 	//% group="5. Low level  and debug functions:"
@@ -231,8 +225,8 @@ namespace SIM700x {
 	}
 
 	/**
-    	* Send plain AT command to modem and return response from it
-    	*/
+	* Send plain AT command to modem and return response from it
+	*/
 	//% weight=100 blockId="SendATCommand"
 	//% block="SIM700x SendATCommand %atCommand || timeout:%timeout"
 	//% timeout.defl=1000 expandableArgumentMode="toggle"
