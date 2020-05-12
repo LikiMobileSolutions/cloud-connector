14e7bf63603d8aa8ae05be51e1a57447f1e11b57
Maciek Sałata <maciej.salata@likims.com>
2020-05-12 21:00:14 +0200

    Standardise development process:
    * Added idea configs
    * Added .editorconfig
    * Source code formatted

--- utils.ts
+++ utils.ts
@@ -1,18 +1,18 @@
 function trimString(stringToTrim: string) {
-    let sliceBeginIndex = 0
-    let sliceEndIndex = 0
+  let sliceBeginIndex = 0
+  let sliceEndIndex = 0
 
-    for (let i = 0; i < stringToTrim.length; i++) {
-        if (stringToTrim.charAt(i) != " " && stringToTrim.charCodeAt(i) != 13 && stringToTrim.charCodeAt(i) != 10) {
-            sliceBeginIndex = i
-            break
-        }
+  for (let i = 0; i < stringToTrim.length; i++) {
+    if (stringToTrim.charAt(i) != " " && stringToTrim.charCodeAt(i) != 13 && stringToTrim.charCodeAt(i) != 10) {
+      sliceBeginIndex = i
+      break
     }
-    for (let j = stringToTrim.length; j >= 0; j--) {
-        if (stringToTrim.charAt(j-1) != " " && stringToTrim.charCodeAt(j-1) != 13 && stringToTrim.charCodeAt(j-1) != 10) {
-            sliceEndIndex = j
-            break
-        }
+  }
+  for (let j = stringToTrim.length; j >= 0; j--) {
+    if (stringToTrim.charAt(j - 1) != " " && stringToTrim.charCodeAt(j - 1) != 13 && stringToTrim.charCodeAt(j - 1) != 10) {
+      sliceEndIndex = j
+      break
     }
-    return stringToTrim.slice(sliceBeginIndex, sliceEndIndex)
+  }
+  return stringToTrim.slice(sliceBeginIndex, sliceEndIndex)
 }
